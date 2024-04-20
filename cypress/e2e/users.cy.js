9ad3596786d091268599ba77d8142e6971a6a160
Ériko Fernando <eriko.bgoncalves@gmail.com>
2024-04-20 10:05:09 -0300

    Atualizações

--- cypress/e2e/users.cy.js
+++ cypress/e2e/users.cy.js
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker';
+const { METHOD_HTTP } = require('../support/method-http.js');
 describe('Teste da rota /users', function () {
   describe('Teste de Bad request', function () {
     it('Deve rebecer Bad request ao cadastrar um usuário sem e-mail', function () {
@@ -12,7 +13,7 @@ describe('Teste da rota /users', function () {
       }).then(function (response) {
         expect(response.status).to.equal(400);
         expect(response.body).to.be.undefined;
-      })
+      });
     });
 
     it('Deve receber Bad request ao cadastrar um usuário sem nome', function () {
@@ -101,12 +102,12 @@ describe('Teste da rota /users', function () {
     before(function () {
       cy.criarUsuario().then(function (dados) {
         usuarioCriado = dados;
-      })
+      });
     });
 
     after(function () {
       cy.deletarUsuario(usuarioCriado.id);
-    })
+    });
 
     it('Deve ser possível consultar um usuário por id', function () {
       cy.request('/users/' + usuarioCriado.id).then(function (response) {
@@ -124,8 +125,8 @@ describe('Teste da rota /users', function () {
       }).then(function (response) {
         expect(response.status).to.equal(404);
         expect(response.body).to.be.empty;
-      })
-    })
+      });
+    });
 
     it('Deve receber BAD REQUEST ao consultar um ID inválido', function () {
       cy.request({
@@ -135,16 +136,52 @@ describe('Teste da rota /users', function () {
       }).then(function (response) {
         expect(response.status).to.equal(400);
         expect(response.body).to.be.empty;
-      })
-    })
+      });
+    });
 
     it('Deve ser possível consultar a lista de todos os usuários', function () {
       cy.request('/users').then(function (response) {
         expect(response.status).to.equal(200);
         expect(response.body).to.be.an('array');
         expect(response.body).to.deep.includes(usuarioCriado);
-      })
-    })
+      });
+    });
+  });
+
+  describe.only('Testes relacionados a atualização de usuários', function () {
+    var usuarioCriado;
+
+    before(function () {
+      cy.criarUsuario().then(function (dados) {
+        usuarioCriado = dados;
+      });
+    });
+
+    after(function () {
+      cy.deletarUsuario(usuarioCriado.id);
+    });
+
+    it('Atualizar usuário com sucesso', function () {
+      var userComId = '/users/' + usuarioCriado.id
+      var bodyRequisicao = {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+      }
+
+      cy.request({ method: METHOD_HTTP.PUT, url: userComId, body: bodyRequisicao }).then(function (response) {
+        var bodyToAssert = {
+          id: usuarioCriado.id,
+          name: bodyRequisicao.name,
+          email: bodyRequisicao.email,
+          createdAt: response.body.createdAt,
+          updatedAt: response.body.updatedAt
+        }
+        expect(response.status).to.be.equal(200);
+        expect(response.body.createdAt).to.be.an('string');
+        expect(response.body.updatedAt).to.be.an('string');
+        expect(response.body).to.deep.equal(bodyToAssert);
+      });
+    });
   });
 });
 
---
+++ cypress/support/method-http.js
@@ -0,0 +1,3 @@
+export const METHOD_HTTP = {
+    PUT: 'PUT'
+}
